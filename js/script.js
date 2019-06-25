deabd4109b2b1ab8ecdb4025c196257b581c9316
Aleksandr <markovkin88@gmail.com>
2019-06-25 19:27:12 +0300

    Добавляет верстку модального окна. Добавляет оверлей

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 // Раздел переменных
 var show = document.querySelector(".catalog-list");
+var over = document.querySelector(".overlay");
 
+// Задержка показа выпадающего меню раздела "каталог"
 function myFunction() {
   show.classList.toggle("show");
+}
+
+// Функциии включения оверлея
+function on() {
+  over.style.display = "block";
+}
+
+function off() {
+  over.style.display = "none";
 }
