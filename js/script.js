198c0799e3b9d55324727a9221cefcc57f480b78
Aleksandr <markovkin88@gmail.com>
2019-06-25 19:42:06 +0300

    Добавляет функцию открытия/закрытия модального окна

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 // Раздел переменных
 var show = document.querySelector(".catalog-list");
+var modal = document.querySelector(".modal");
 var over = document.querySelector(".overlay");
 
 // Задержка показа выпадающего меню раздела "каталог"
@@ -10,8 +11,24 @@ function myFunction() {
 // Функциии включения оверлея
 function on() {
   over.style.display = "block";
+  modal.style.display = "block";
 }
 
 function off() {
   over.style.display = "none";
+  modal.style.display = "none";
 }
+
+function modalClose() {
+  modal.style.display = "none";
+  over.style.display = "none";
+}
+
+// Закрыть оверлей и модального окна по нажатие ESC
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    modal.style.display = "none";
+    over.style.display = "none";
+  }
+});
